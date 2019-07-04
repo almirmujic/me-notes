db9b77dc3c0a9a766adfef6c44016b5c97315676
Almir <contact.almir@gmail.com>
2019-07-03 21:28:17 -0500

    Require express, cors, and dotenv packages
    Start up simple server on given port - Set up Express to parse JSON data

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// require express and cors packages
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
