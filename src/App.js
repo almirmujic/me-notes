10bd08059b1d6b09d9609b0956729e15b5086253
Almir <contact.almir@gmail.com>
2019-07-04 15:27:36 -0500

    Import components and Route them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//components
+import Navbar from './components/Navbar';
+import Notes from './components/Notes';
+import EditNote from './components/EditNote';
+import CreateNote from './components/CreateNote';
+import CreateTitle from './components/CreateTitle';
+
+//styling
 import './App.css';
 
 function App() {
-  return <Router />;
+  return (
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={Notes} />
+      <Route path="/edit/:id" exact component={EditNote} />
+      <Route path="/create" exact component={CreateNote} />
+      <Route path="/title" exact component={CreateTitle} />
+    </Router>
+  );
 }
 
 export default App;
