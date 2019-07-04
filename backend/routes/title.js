ab234f3d4be1c4c187ad6a746b29323eae9d9d19
Almir <contact.almir@gmail.com>
2019-07-03 22:42:35 -0500

    Create POST request to create new titles and save it to database

--- backend/routes/title.js
+++ backend/routes/title.js
@@ -7,3 +7,17 @@ router.route('/').get((request, response) => {
     .then(titles => response.json(titles))
     .catch(err => response.status(400).json('Error: ' + err));
 });
+
+// baseurl.com/title/add - POST request
+router.route('/add').post((request, response) => {
+  const title = request.body.title;
+
+  const newTitle = new Title({ title });
+
+  newTitle
+    .save()
+    .then(() => response.json('Title added...'))
+    .catch(err => response.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
