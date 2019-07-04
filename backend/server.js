36efa6adc5e011bd164e97a70fdc4c31cd67957c
Almir <contact.almir@gmail.com>
2019-07-03 21:41:30 -0500

    Require mongoose to help connect mongoDB
    Pull in environment variable to successfully connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // require express and cors packages
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -9,6 +10,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+const config = { useNewUrlParser: true, useCreateIndex: true };
+mongoose.connect(uri, config);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
