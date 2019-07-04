101255fb5e201d6dd1bb563ce2538469ae33bb13
Almir <contact.almir@gmail.com>
2019-07-04 15:19:22 -0500

    Remove default CRA app container and header
    Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
