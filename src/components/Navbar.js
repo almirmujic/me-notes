7f7da85707c58fef973447ad89e6ebfd2658d81f
Almir <contact.almir@gmail.com>
2019-07-04 19:38:56 -0500

    Create Tab from Material UI linking to other components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import { Link } from 'react-router-dom';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired
+};
 
 const useStyles = makeStyles({
   root: {
@@ -13,16 +28,24 @@ const useStyles = makeStyles({
 
 export default function Navbar() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
   return (
     <div className={classes.root}>
       <AppBar position="static" color="default">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h6" color="primary">
             Me-notes
           </Typography>
-          <Link to="/create"> Create note </Link>
         </Toolbar>
+        <Tabs variant="fullWidth" value={value} onChange={handleChange}>
+          <Tab label="Notes" component={Link} to="/" />
+          <Tab label="Create Note" component={Link} to="/create" />
+          <Tab label="Create Title" component={Link} to="/title" />
+        </Tabs>
       </AppBar>
     </div>
   );
