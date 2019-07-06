4ab75203ee2047bfc291393ae5a981768dd2c913
Almir <contact.almir@gmail.com>
2019-07-06 12:15:01 -0500

    Remove create title route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Navbar from './components/Navbar';
 import Notes from './components/Notes';
 import CreateNote from './components/CreateNote';
 import EditNote from './components/EditNote';
-import CreateTitle from './components/CreateTitle';
 
 //styling
 import './App.css';
@@ -20,7 +19,6 @@ function App() {
         <Route path="/" exact component={Notes} />
         <Route path="/create" exact component={CreateNote} />
         <Route path="/edit/:id" exact component={EditNote} />
-        <Route path="/title" exact component={CreateTitle} />
         <Redirect to="/" />
       </Switch>
     </React.Fragment>
