d996845eda3fb839bc0104b6ded13f95b90eb4cb
Almir <contact.almir@gmail.com>
2019-07-06 15:05:08 -0500

    Removed link and button - unused

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Note from './Note';
 
 // Material UI
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 
