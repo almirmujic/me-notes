d1c8704e35cc02311a7d7e04e40121e2512c9a06
Almir <contact.almir@gmail.com>
2019-07-05 20:05:16 -0500

    Create clean up function inside useEffect to fetch notes asynchronously

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Notes() {
-  return <div>These are the notes components</div>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const result = await axios('http://localhost:5000/note');
+      setNotes(result.data);
+    };
+    fetchNotes();
+  }, []);
+
+  return <div />;
 }
