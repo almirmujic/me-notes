e73f8598372c561378c15aff8973ef2cc492f828
Almir <contact.almir@gmail.com>
2019-07-06 11:29:46 -0500

    Map over fetched notes and display it when returned

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-export default function Notes() {
+function Notes() {
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    const fetchNotes = async () => {
-      const result = await axios('http://localhost:5000/note');
-      setNotes(result.data);
+    const fetchNotes = () => {
+      axios.get('http://localhost:5000/note').then(res => setNotes(res.data));
     };
+
     fetchNotes();
   }, []);
 
@@ -18,8 +18,23 @@ export default function Notes() {
       .delete('http://localhost:5000/note/' + id)
       .then(res => console.log(res.data));
 
-    setNotes(notes.filter(note => note._id !== id));
+    setNotes({ notes: notes.filter(note => note._id !== id) });
   };
 
-  return <div />;
+  const displayData = notes.map((note, id) => (
+    <div style={{ paddingBottom: '1rem' }} key={id}>
+      <p>Title: {note.title}</p>
+      <span>Note: {note.body}</span>
+      <p>Date: {note.date}</p>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h3>Notes</h3>
+      {displayData}
+    </div>
+  );
 }
+
+export default Notes;
